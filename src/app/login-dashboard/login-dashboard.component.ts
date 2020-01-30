29d5eb5ffdbeeb69058e04c82f0c8e32bd61a4e5
kushpranjale <kush13feb@gmail.com>
2020-01-30 18:59:06 +0530

    #kush  responsive login page

--- src/app/login-dashboard/login-dashboard.component.ts
+++ src/app/login-dashboard/login-dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-dashboard',
   templateUrl: './login-dashboard.component.html',
-  styleUrls: ['./login-dashboard.component.css']
+  styleUrls: ['./login-dashboard.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginDashboardComponent implements OnInit {
 
